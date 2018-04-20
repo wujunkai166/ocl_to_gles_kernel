c86f5ecf281ab00311d8828bdbbe9c8e20c87f1d
Junkai Wu <junkai.wu@intel.com>
2018-04-20 01:59:12 -0400

    Add kernel: kernel_gauss_lap_reconstruct

--- kernel_gauss_lap_reconstruct/kernel_gauss_lap_reconstruct.cpp
+++ kernel_gauss_lap_reconstruct/kernel_gauss_lap_reconstruct.cpp
@@ -16,31 +16,28 @@ static const char gComputeShader_y[] =
     "layout(local_size_x = 8, local_size_y = 4) in;\n"
     "layout(binding = 0) readonly buffer Input0 {\n"
     "    vec4 data[];\n"
-    "} input_gauss0;\n"
+    "} input_lap;\n"
     "layout(binding = 1) readonly buffer Input1 {\n"
     "    int value;\n"
-    "} input_gauss0_w;\n"
+    "} input_lap_w;\n"
     "layout(binding = 2) readonly buffer Input2 {\n"
-    "    int value;\n"
-    "} gauss0_offset_x;\n"
-    "layout(binding = 3) readonly buffer Input3 {\n"
     "    vec2 data[];\n"
-    "} input_gauss1;\n"
+    "} input_gauss;\n"
+    "layout(binding = 3) readonly buffer Input3 {\n"
+    "    int value;\n"
+    "} input_gauss_w;\n"
     "layout(binding = 4) readonly buffer Input4 {\n"
     "    int value;\n"
-    "} input_gauss1_w;\n"
-    "layout(binding = 5) readonly buffer Input5 {\n"
-    "    int value;\n"
-    "} input_gauss1_h;\n"
-    "layout(binding = 6) writeonly buffer Output {\n"
+    "} input_gauss_h;\n"
+    "layout(binding = 5) writeonly buffer Output {\n"
     "    vec4 data[];\n"
     "} output0;\n"
-    "layout(binding = 7) readonly buffer Input6 {\n"
+    "layout(binding = 6) readonly buffer Input5 {\n"
     "    int value;\n"
     "} output0_w;\n"
-    "layout(binding = 8) readonly buffer Input7 {\n"
+    "layout(binding = 7) readonly buffer Input6 {\n"
     "    int value;\n"
-    "} lap_offset_x;\n"
+    "} out_offset_x;\n"
     "void main()\n"
     "{\n"
     "    int g_x = int(gl_GlobalInvocationID.x);\n"
@@ -51,25 +48,25 @@ static const char gComputeShader_y[] =
     "    vec2 result_cur[2] = vec2[](vec2(0.0f), vec2(0.0f));\n"
     "    vec2 result_next[2] = vec2[](vec2(0.0f), vec2(0.0f));\n"
     "    vec4 final_g;\n"
-    "    vec4 lap;"
+    "    vec4 out_data;\n"
     "    vec2 tmp_data;\n"
     "    int i_ver;\n"
     "\n"
     "    float coeffs[6] = float[](0.0f, 0.289f, 0.5f, 0.422f, 0.5f, 0.289f);\n"
     "\n"
     "    for (i_ver = -1; i_ver <= 1; i_ver++) {\n"
-    "        int cur_g_y = clamp(g_y / 2 + i_ver, 0, input_gauss1_h.value - 1);\n"
-    "        int pre_x = clamp(cur_x - 1, 0, input_gauss1_w.value - 1);\n"
-    "        int next_x = clamp(cur_x + 1, 0, input_gauss1_w.value - 1);\n"
+    "        int cur_g_y = clamp(g_y / 2 + i_ver, 0, input_gauss_h.value - 1);\n"
+    "        int pre_x = clamp(cur_x - 1, 0, input_gauss_w.value - 1);\n"
+    "        int next_x = clamp(cur_x + 1, 0, input_gauss_w.value - 1);\n"
     "        float coeff0 = coeffs[(i_ver + 1) * 2 + 1];\n"
     "        float coeff1 = coeffs[(i_ver + 1) * 2];\n"
-    "        tmp_data = input_gauss1.data[cur_g_y * input_gauss1_w.value + pre_x];\n"
+    "        tmp_data = input_gauss.data[cur_g_y * input_gauss_w.value + pre_x];\n"
     "        result_pre[0] += tmp_data * coeff0;\n"
     "        result_pre[1] += tmp_data * coeff1;\n"
-    "        tmp_data = input_gauss1.data[cur_g_y * input_gauss1_w.value + cur_x];\n"
+    "        tmp_data = input_gauss.data[cur_g_y * input_gauss_w.value + cur_x];\n"
     "        result_cur[0] += tmp_data * coeff0;\n"
     "        result_cur[1] += tmp_data * coeff1;\n"
-    "        tmp_data = input_gauss1.data[cur_g_y * input_gauss1_w.value + next_x];\n"
+    "        tmp_data = input_gauss.data[cur_g_y * input_gauss_w.value + next_x];\n"
     "        result_next[0] += tmp_data * coeff0;\n"
     "        result_next[1] += tmp_data * coeff1;\n"
     "    }\n"
@@ -82,9 +79,10 @@ static const char gComputeShader_y[] =
     "        final_g.xz = tmp_result.xy;\n"
     "        final_g.yw = tmp_result.xy * coeffs[2] + tmp_result.yz * coeffs[4];\n"
     "        final_g = clamp (final_g + 0.5f, 0.0f, 255.0f);\n"
-    "        lap = (input_gauss0.data[(g_y + i_line) * input_gauss0_w.value + g_x + gauss0_offset_x.value] - final_g) * 0.5f + 128.0f + 0.5f;\n"
-    "        lap = clamp (lap, 0.0f, 255.0f);\n"
-    "        output0.data[(g_y + i_line) * output0_w.value + g_x + lap_offset_x.value] = lap;\n"
+    "        vec4 lap = input_lap.data[(g_y + i_line) * input_lap_w.value + g_x];\n"
+    "        lap = (lap - 128.0f) * 2.0f;\n"
+    "        out_data = final_g + lap + 0.5f;\n"
+    "        output0.data[(g_y + i_line) * output0_w.value + g_x + out_offset_x.value] = out_data;\n"
     "    }\n"
     "}\n";
 
@@ -93,31 +91,28 @@ static const char gComputeShader_uv[] =
     "layout(local_size_x = 8, local_size_y = 2) in;\n"
     "layout(binding = 0) readonly buffer Input0 {\n"
     "    vec4 data[];\n"
-    "} input_gauss0;\n"
+    "} input_lap;\n"
     "layout(binding = 1) readonly buffer Input1 {\n"
     "    int value;\n"
-    "} input_gauss0_w;\n"
+    "} input_lap_w;\n"
     "layout(binding = 2) readonly buffer Input2 {\n"
-    "    int value;\n"
-    "} gauss0_offset_x;\n"
-    "layout(binding = 3) readonly buffer Input3 {\n"
     "    vec2 data[];\n"
-    "} input_gauss1;\n"
+    "} input_gauss;\n"
+    "layout(binding = 3) readonly buffer Input3 {\n"
+    "    int value;\n"
+    "} input_gauss_w;\n"
     "layout(binding = 4) readonly buffer Input4 {\n"
     "    int value;\n"
-    "} input_gauss1_w;\n"
-    "layout(binding = 5) readonly buffer Input5 {\n"
-    "    int value;\n"
-    "} input_gauss1_h;\n"
-    "layout(binding = 6) writeonly buffer Output {\n"
+    "} input_gauss_h;\n"
+    "layout(binding = 5) writeonly buffer Output {\n"
     "    vec4 data[];\n"
     "} output0;\n"
-    "layout(binding = 7) readonly buffer Input6 {\n"
+    "layout(binding = 6) readonly buffer Input5 {\n"
     "    int value;\n"
     "} output0_w;\n"
-    "layout(binding = 8) readonly buffer Input7 {\n"
+    "layout(binding = 7) readonly buffer Input6 {\n"
     "    int value;\n"
-    "} lap_offset_x;\n"
+    "} out_offset_x;\n"
     "void main()\n"
     "{\n"
     "    int g_x = int(gl_GlobalInvocationID.x);\n"
@@ -129,29 +124,29 @@ static const char gComputeShader_uv[] =
     "    vec2 result_next1[2] = vec2[](vec2(0.0f), vec2(0.0f));\n"
     "    vec2 result_next2[2] = vec2[](vec2(0.0f), vec2(0.0f));\n"
     "    vec4 final_g;\n"
-    "    vec4 lap;\n"
+    "    vec4 out_data;\n"
     "    vec2 tmp_data;\n"
     "    int i_ver;\n"
     "\n"
     "    float coeffs[6] = float[](0.0f, 0.289f, 0.5f, 0.422f, 0.5f, 0.289f);\n"
     "\n"
     "    for (i_ver = -1; i_ver <= 1; i_ver++) {\n"
-    "        int cur_g_y = clamp(g_y / 2 + i_ver, 0, input_gauss1_h.value - 1);\n"
-    "        int pre_x = clamp(cur_x - 1, 0, input_gauss1_w.value - 1);\n"
-    "        int next1_x = clamp(cur_x + 1, 0, input_gauss1_w.value - 1);\n"
-    "        int next2_x = clamp(cur_x + 2, 0, input_gauss1_w.value - 1);\n"
+    "        int cur_g_y = clamp(g_y / 2 + i_ver, 0, input_gauss_h.value - 1);\n"
+    "        int pre_x = clamp(cur_x - 1, 0, input_gauss_w.value - 1);\n"
+    "        int next1_x = clamp(cur_x + 1, 0, input_gauss_w.value - 1);\n"
+    "        int next2_x = clamp(cur_x + 2, 0, input_gauss_w.value - 1);\n"
     "        float coeff0 = coeffs[(i_ver + 1) * 2 + 1];\n"
     "        float coeff1 = coeffs[(i_ver + 1) * 2];\n"
-    "        tmp_data = input_gauss1.data[cur_g_y * input_gauss1_w.value + pre_x];\n"
+    "        tmp_data = input_gauss.data[cur_g_y * input_gauss_w.value + pre_x];\n"
     "        result_pre[0] += tmp_data * coeff0;\n"
     "        result_pre[1] += tmp_data * coeff1;\n"
-    "        tmp_data = input_gauss1.data[cur_g_y * input_gauss1_w.value + cur_x];\n"
+    "        tmp_data = input_gauss.data[cur_g_y * input_gauss_w.value + cur_x];\n"
     "        result_cur[0] += tmp_data * coeff0;\n"
     "        result_cur[1] += tmp_data * coeff1;\n"
-    "        tmp_data = input_gauss1.data[cur_g_y * input_gauss1_w.value + next1_x];\n"
+    "        tmp_data = input_gauss.data[cur_g_y * input_gauss_w.value + next1_x];\n"
     "        result_next1[0] += tmp_data * coeff0;\n"
     "        result_next1[1] += tmp_data * coeff1;\n"
-    "        tmp_data = input_gauss1.data[cur_g_y * input_gauss1_w.value + next2_x];\n"
+    "        tmp_data = input_gauss.data[cur_g_y * input_gauss_w.value + next2_x];\n"
     "        result_next2[0] += tmp_data * coeff0;\n"
     "        result_next2[1] += tmp_data * coeff1;\n"
     "    }\n"
@@ -164,9 +159,10 @@ static const char gComputeShader_uv[] =
     "        final_g.xy = tmp_result.xy;\n"
     "        final_g.zw = tmp_result.xy * coeffs[2] + tmp_result.zw * coeffs[4];\n"
     "        final_g = clamp (final_g + 0.5f, 0.0f, 255.0f);\n"
-    "        lap = (input_gauss0.data[(g_y + i_line) * input_gauss0_w.value + g_x + gauss0_offset_x.value] - final_g) * 0.5f + 128.0f + 0.5f;\n"
-    "        lap = clamp (lap, 0.0f, 255.0f);\n"
-    "        output0.data[(g_y + i_line) * output0_w.value + g_x + lap_offset_x.value] = lap;\n"
+    "        vec4 lap = input_lap.data[(g_y + i_line) * input_lap_w.value + g_x];\n"
+    "        lap = (lap - 128.0f) * 2.0f;\n"
+    "        out_data = final_g + lap + 0.5f;\n"
+    "        output0.data[(g_y + i_line) * output0_w.value + g_x + out_offset_x.value] = out_data;\n"
     "    }\n"
     "}\n";
 
@@ -244,41 +240,6 @@ void setupSSBufferObject(GLuint& ssbo, GLuint index, void* pIn, GLuint buf_size)
     glBindBufferBase(GL_SHADER_STORAGE_BUFFER, index, ssbo);
 }
 
-void convert_bgr_to_nv12 (float* input, float* output_y, float* output_uv, int width, int height)
-{
-    float *y = (float *) malloc (width * height * sizeof (float));
-    float *u = (float *) malloc (width * height * sizeof (float));
-    float *v = (float *) malloc (width * height * sizeof (float));
-
-    for(int row = 0; row < height; row++) {
-        for(int col = 0; col < width; col++) {
-            int index = row * width + col;
-            float b = input[index * 3];
-            float g = input[index * 3 + 1];
-            float r = input[index * 3 + 2];
-
-            y[index] = 0.299f * r + 0.587f * g + 0.114f * b;
-            u[index] = -0.169f * r - 0.331f * g + 0.5f * b + 128;
-            v[index] = 0.5f * r - 0.419f * g - 0.081f * b + 128;
-
-            output_y[index] = y[index];
-        }
-    }
-
-    for(int row = 0; row < height / 2; row++) {
-        for(int col = 0; col < width / 2; col++) {
-            int index = row * width / 2 + col;
-
-            output_uv[index * 2] = u[row * 2 * width + col * 2];
-            output_uv[index * 2 + 1] = v[row * 2 * width + col * 2];
-        }
-    }
-
-    free(y);
-    free(u);
-    free(v);
-}
-
 void tryComputeShader()
 {
     GLuint computeProgram;
@@ -303,16 +264,21 @@ void tryComputeShader()
     printf("w:%d, h:%d\n", image_w, image_h);
 
     GLuint image_size = image_w * image_h;
-    float* input0_bgr = (float *)malloc(image_size * 3 * sizeof(float));
-    float* input0_y = (float *)malloc(image_size * sizeof(float));
-    float* input0_uv = (float *)malloc(image_size / 2 * sizeof(float));
 
     uint8_t* input_gauss_nv12 = (uint8_t *)malloc(image_size * 3 / 8 * sizeof(uint8_t));
     float* input_gauss_y = (float *)malloc(image_size / 4 * sizeof(float));
-    float* input_gauss_uv = (float *)malloc(image_size / 8 * sizeof(float));    
+    float* input_gauss_uv = (float *)malloc(image_size / 8 * sizeof(float));
+
+    uint8_t* input_lap_nv12 = (uint8_t *)malloc(image_size * 3 / 2 * sizeof(uint8_t));
+    float* input_lap_y = (float *)malloc(image_size * sizeof(float));
+    float* input_lap_uv = (float *)malloc(image_size / 2 * sizeof(float));
 
     FILE* fp = fopen("input_gauss.raw", "r");
     fread(input_gauss_nv12, 1, image_size * 3 / 8, fp);
+    fclose(fp);
+
+    fp = fopen("input_lap.raw", "r");
+    fread(input_lap_nv12, 1, image_size * 3 / 2, fp);
     fclose(fp);
 
     uint8_t* output_nv12 = (uint8_t *)malloc(image_size * 3 / 2 * sizeof(uint8_t));
@@ -325,27 +291,27 @@ void tryComputeShader()
         input_gauss_uv[i] = (float)input_gauss_nv12[i + image_size / 4];
     }
 
-    for (GLuint i = 0; i < image_size * 3; i++) {
-        input0_bgr[i] = (float)input0_image.data[i];
+    for (GLuint i = 0; i < image_size; i++) {
+        input_lap_y[i] = (float)input_lap_nv12[i];
     }
 
-    convert_bgr_to_nv12 (input0_bgr, input0_y, input0_uv, image_w, image_h);
+    for (GLuint i = 0; i < image_size / 2; i++) {
+        input_lap_uv[i] = (float)input_lap_nv12[i + image_size];
+    }
 
     int input_w = image_w / 4;
     int input_h = image_h / 2;
     int output_w = image_w / 4;
-    int in_offset_x = 0;
-    int lap_offset_x = 0;
+    int out_offset_x = 0;
 
-    setupSSBufferObject(input0_ssbo, 0, (void *)input0_y, image_size * sizeof(float));
+    setupSSBufferObject(input0_ssbo, 0, (void *)input_lap_y, image_size * sizeof(float));
     setupSSBufferObject(input1_ssbo, 1, (void *)&input_w, sizeof(int));
-    setupSSBufferObject(input2_ssbo, 2, (void *)&in_offset_x, sizeof(int));
-    setupSSBufferObject(input3_ssbo, 3, (void *)input_gauss_y, image_size / 4 * sizeof(float));
-    setupSSBufferObject(input4_ssbo, 4, (void *)&input_w, sizeof(int));
-    setupSSBufferObject(input5_ssbo, 5, (void *)&input_h, sizeof(int));
-    setupSSBufferObject(output0_ssbo, 6, NULL, image_size * sizeof(float));
-    setupSSBufferObject(input6_ssbo, 7, (void *)&output_w, sizeof(int));
-    setupSSBufferObject(input7_ssbo, 8, (void *)&lap_offset_x, sizeof(int));
+    setupSSBufferObject(input2_ssbo, 2, (void *)input_gauss_y, image_size / 4 * sizeof(float));
+    setupSSBufferObject(input3_ssbo, 3, (void *)&input_w, sizeof(int));
+    setupSSBufferObject(input4_ssbo, 4, (void *)&input_h, sizeof(int));
+    setupSSBufferObject(output0_ssbo, 5, NULL, image_size * sizeof(float));
+    setupSSBufferObject(input5_ssbo, 6, (void *)&output_w, sizeof(int));
+    setupSSBufferObject(input6_ssbo, 7, (void *)&out_offset_x, sizeof(int));
 
     CHECK();
 
@@ -384,15 +350,14 @@ void tryComputeShader()
 
     input_h = image_h / 4;
 
-    setupSSBufferObject(input0_ssbo, 0, (void *)input0_uv, image_size / 2 * sizeof(float));
+    setupSSBufferObject(input0_ssbo, 0, (void *)input_lap_uv, image_size / 2 * sizeof(float));
     setupSSBufferObject(input1_ssbo, 1, (void *)&input_w, sizeof(int));
-    setupSSBufferObject(input2_ssbo, 2, (void *)&in_offset_x, sizeof(int));
-    setupSSBufferObject(input3_ssbo, 3, (void *)input_gauss_uv, image_size / 8 * sizeof(float));
-    setupSSBufferObject(input4_ssbo, 4, (void *)&input_w, sizeof(int));
-    setupSSBufferObject(input5_ssbo, 5, (void *)&input_h, sizeof(int));
-    setupSSBufferObject(output0_ssbo, 6, NULL, image_size / 2 * sizeof(float));
-    setupSSBufferObject(input6_ssbo, 7, (void *)&output_w, sizeof(int));
-    setupSSBufferObject(input7_ssbo, 8, (void *)&lap_offset_x, sizeof(int));
+    setupSSBufferObject(input3_ssbo, 2, (void *)input_gauss_uv, image_size / 8 * sizeof(float));
+    setupSSBufferObject(input4_ssbo, 3, (void *)&input_w, sizeof(int));
+    setupSSBufferObject(input5_ssbo, 4, (void *)&input_h, sizeof(int));
+    setupSSBufferObject(output0_ssbo, 5, NULL, image_size / 2 * sizeof(float));
+    setupSSBufferObject(input6_ssbo, 6, (void *)&output_w, sizeof(int));
+    setupSSBufferObject(input7_ssbo, 7, (void *)&out_offset_x, sizeof(int));
 
     CHECK();
 
@@ -425,10 +390,10 @@ void tryComputeShader()
 
     glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
     glDeleteProgram(computeProgram);
-
-    free(input0_bgr);
-    free(input0_y);
-    free(input0_uv);
+    
+    free(input_lap_nv12);
+    free(input_lap_y);
+    free(input_lap_uv);
     free(output_nv12);
     free(input_gauss_nv12);
     free(input_gauss_y);
